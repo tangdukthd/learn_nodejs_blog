aadd73910aa9fe06ddedb9fcc99d234a33be94b0
Tăng Du <tangdu.kthd@gmail.com>
2022-10-28 16:43:46 +0700

    third commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.use(morgan('combined'))
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
