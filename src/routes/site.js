01114d4a2826edfe1341a2bb6be094237c001043
Tăng Du <tangdu.kthd@gmail.com>
2022-11-22 20:49:30 +0700

    course-detail-page

--- src/routes/site.js
+++ src/routes/site.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const SiteController = require('../app/controllers/SiteController');
 
-router.use('/search', SiteController.search);
-router.use('/', SiteController.home);
+router.get('/search', SiteController.search);
+router.get('/', SiteController.home);
 
 module.exports = router;
