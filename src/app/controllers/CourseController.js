fbf56cb41444a4e68ab15f2f1829de2409ef390f
Tăng Du <tangdu.kthd@gmail.com>
2022-11-23 18:49:27 +0700

    course detail page

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,13 +1,18 @@
 const Course = require('../models/Course');
-const { multipleMongooseToObject } = require('../../util/mongoose');
+const {
+    multipleMongooseToObject,
+    mongooseToObject,
+} = require('../../util/mongoose');
 
 class CourseController {
     //[GET] /courses/:slug
     show(req, res, next) {
         Course.findOne({ slug: req.params.slug })
-            .then((course) => {
-                res.render('courses/show');
-            })
+            .then((course) =>
+                res.render('courses/show', {
+                    course: mongooseToObject(course),
+                }),
+            )
             .catch(next);
     }
 }
