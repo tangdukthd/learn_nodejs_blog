dc5ac15002457d37b4acb1920b8b7250db19d1b6
Tăng Du <tangdu.kthd@gmail.com>
2022-12-05 16:28:48 +0700

    Select all with checkbox

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -66,6 +66,18 @@ class CourseController {
             .then(() => res.redirect('back'))
             .catch(next);
     }
+    //[POST] /courses/handle-form-action
+    handlerFormAction(req, res, next) {
+        switch (req.body.action) {
+            case 'delete':
+                Course.delete({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            default:
+                res.json({ message: 'Action is invalid' });
+        }
+    }
 }
 
 module.exports = new CourseController();
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -5,6 +5,7 @@ const CourseController = require('../app/controllers/CourseController');
 router.get('/create', CourseController.create);
 router.get('/:id/edit', CourseController.edit);
 router.post('/store', CourseController.store);
+router.post('/handle-form-action', CourseController.handlerFormAction);
 router.put('/:id', CourseController.update);
 router.patch('/:id/restore', CourseController.restore);
 router.delete('/:id', CourseController.delete);
